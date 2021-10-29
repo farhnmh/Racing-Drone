7a0c29323a0f378f72433cd73c3fd244097e1511
Farhan Muhammad <farhnmh@gmail.com>
2021-10-29 17:01:49 +0700

    Upload Project

---
+++ Assets/Scripts/ArduinoManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO.Ports;
+using UnityEngine;
+
+public class ArduinoManager : MonoBehaviour
+{
+    [Header ("Arduino Attribute")]
+    [SerializeField] bool isConnected;
+    [SerializeField] string portName;
+    [SerializeField] int baudRate;
+    [SerializeField] SerialPort data_stream;
+    public List<string> joystickData;
+    
+    void Start()
+    {
+        //deklarasi sumber port dan baud rate yang digunakan oleh Arduino
+        data_stream = new SerialPort(portName, baudRate);
+
+        //membuka port yang terhubung
+        data_stream.Open();
+
+        //menentukan batas jumlah waktu yang digunakan saat proses tidak selesai
+        data_stream.ReadTimeout = 101;
+    }
+
+    void Update()
+    {
+        if (data_stream.IsOpen)
+        {
+            try
+            {
+                //menerima data yang dikirimkan oleh Arduino
+                string[] datas = data_stream.ReadLine().Split(',');
+
+                //menyiapkan seluruh data yang akan digunakan
+                joystickData[0] = datas[0];
+                joystickData[1] = datas[1];
+                joystickData[2] = datas[2];
+            }
+            catch (System.Exception)
+            {
+
+            }
+        }
+        else
+        {
+            //menutup port
+            data_stream.Close();
+            isConnected = false;
+        }
+    }
+}
---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField] Transform drone;
+    [SerializeField] Vector3 offset;
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = new Vector3(drone.position.x - offset.x,
+                                              drone.position.y - offset.y,
+                                              drone.position.z - offset.z);
+    }
+}
